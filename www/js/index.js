b507624c3e90b06b27639548867071747931f2bb
Mickelle <mickelled@cybage.com>
2017-06-27 16:14:18 +0530

    Sample add and edit

--- www/js/index.js
+++ www/js/index.js
@@ -28,7 +28,7 @@ var app = {
     // 'pause', 'resume', etc.
     onDeviceReady: function() {
         this.receivedEvent('deviceready');
-      
+       dconsole.log("Test")
       
       
         //==
