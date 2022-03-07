71acad68a3be25c53299ea4aedc2f44233d827b9
Mickelle <mickelled@cybage.com>
2022-03-07 19:14:39 +0530

    removed group

--- test/connectivity/connectivity_test.dart
+++ test/connectivity/connectivity_test.dart
@@ -4,7 +4,6 @@ import 'package:mockito/annotations.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:connectivity_plus/connectivity_plus.dart';
 import 'package:test_flutter_circle_ci/providers/connectivity_provider.dart';
-
 import 'connectivity_test.mocks.dart';
 
 @GenerateMocks([Connectivity, ConnectivityProvider])
@@ -12,39 +11,37 @@ void main() {
   WidgetsFlutterBinding.ensureInitialized();
   MockConnectivity mockConnectivity = MockConnectivity();
 
-  group('Test for connectivity', () {
-    // test(
-    //   'Listening network connectivity changes',
-    //   () async {
-    //     ConnectivityProvider connectivityProvider = ConnectivityProvider();
-    //     expect(connectivityProvider.listenNetworkConnectivity, isA<void>());
-    //   },
-    // );
+  // test(
+  //   'Listening network connectivity changes',
+  //   () async {
+  //     ConnectivityProvider connectivityProvider = ConnectivityProvider();
+  //     expect(connectivityProvider.listenNetworkConnectivity, isA<void>());
+  //   },
+  // );
 
-    // test('Testing CheckNetworkConnectivity method', () async {
-    //   when(mockConnectivity.checkConnectivity())
-    //       .thenAnswer((_) async => ConnectivityResult.wifi);
-    //   ConnectivityProvider connectivityProvider = ConnectivityProvider();
-    //   expect(
-    //     await connectivityProvider.checkNetworkConnectivity(),
-    //     isA<ConnectivityResult>(),
-    //   );
-    // });
+  // test('Testing CheckNetworkConnectivity method', () async {
+  //   when(mockConnectivity.checkConnectivity())
+  //       .thenAnswer((_) async => ConnectivityResult.wifi);
+  //   ConnectivityProvider connectivityProvider = ConnectivityProvider();
+  //   expect(
+  //     await connectivityProvider.checkNetworkConnectivity(),
+  //     isA<ConnectivityResult>(),
+  //   );
+  // });
 
-    test('Testing all the getters', () {
-      final ConnectivityProvider connectivityProvider = ConnectivityProvider();
+  test('Testing all the getters', () {
+    final ConnectivityProvider connectivityProvider = ConnectivityProvider();
 
-      expect(connectivityProvider.getIsConnected, isFalse);
-      expect(connectivityProvider.getShowSnackBar, isFalse);
-      expect(connectivityProvider.getSnackbarKey, isA<GlobalKey>());
-      expect(connectivityProvider.getConnectivity, isA<Connectivity>());
-      expect(connectivityProvider.getResult, isA<ConnectivityResult>());
-    });
-
-    // test('Clears/closes the stream subscription', () {
-    //   ConnectivityProvider connectivityProvider = ConnectivityProvider();
-    //   connectivityProvider.clearConnectivityStream();
-    //   expect(connectivityProvider.clearConnectivityStream, isA<void>());
-    // });
+    expect(connectivityProvider.getIsConnected, isFalse);
+    expect(connectivityProvider.getShowSnackBar, isFalse);
+    expect(connectivityProvider.getSnackbarKey, isA<GlobalKey>());
+    expect(connectivityProvider.getConnectivity, isA<Connectivity>());
+    expect(connectivityProvider.getResult, isA<ConnectivityResult>());
   });
+
+  // test('Clears/closes the stream subscription', () {
+  //   ConnectivityProvider connectivityProvider = ConnectivityProvider();
+  //   connectivityProvider.clearConnectivityStream();
+  //   expect(connectivityProvider.clearConnectivityStream, isA<void>());
+  // });
 }
