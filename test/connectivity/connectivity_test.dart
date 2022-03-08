b836fe2e7bfd97b41161e0341ba4c488acf574ea
Mickelle <mickelled@cybage.com>
2022-03-08 17:08:55 +0530

    all pass

--- test/connectivity/connectivity_test.dart
+++ test/connectivity/connectivity_test.dart
@@ -47,16 +47,16 @@ void main() {
     test('Testing all the getters', () {
       // final ConnectivityProvider connectivityProvider = ConnectivityProvider();
 
-      expect(connectivityProvider.getIsConnected, isFalse);
+      expect(connectivityProvider.getIsConnected, isTrue);
       expect(connectivityProvider.getShowSnackBar, isFalse);
       expect(connectivityProvider.getSnackbarKey, isA<GlobalKey>());
       expect(connectivityProvider.getConnectivity, isA<Connectivity>());
       expect(connectivityProvider.getResult, isA<ConnectivityResult>());
     });
 
-    // test('Clears/closes the stream subscription', () {
-    //   connectivityProvider.clearConnectivityStream();
-    //   expect(connectivityProvider.clearConnectivityStream, isA<void>());
-    // });
+    test('Clears/closes the stream subscription', () {
+      // connectivityProvider.clearConnectivityStream();
+      expect(connectivityProvider.clearConnectivityStream, isA<void>());
+    });
   });
 }
